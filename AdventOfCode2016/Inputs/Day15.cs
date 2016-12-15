abb3059429affc069ee036446062170ce3e33844
Thomas L. Johnsen <t.johnsen@sportradar.com>
2016-12-15 11:34:43 +0100

    Day 15

--- AdventOfCode2016/Inputs/Day15.cs
+++ AdventOfCode2016/Inputs/Day15.cs
@@ -3,7 +3,14 @@ namespace AdventOfCode2016.Inputs
 {
     public class Day15
     {
-        public static string testData = @"";
-        public static string realData = @"";
+        public static string testData = @"Disc #1 has 5 positions; at time=0, it is at position 4.
+Disc #2 has 2 positions; at time=0, it is at position 1.";
+
+        public static string realData = @"Disc #1 has 13 positions; at time=0, it is at position 10.
+Disc #2 has 17 positions; at time=0, it is at position 15.
+Disc #3 has 19 positions; at time=0, it is at position 17.
+Disc #4 has 7 positions; at time=0, it is at position 1.
+Disc #5 has 5 positions; at time=0, it is at position 0.
+Disc #6 has 3 positions; at time=0, it is at position 1.";
     }
 }
--- AdventOfCode2016/Solutions/Day15.cs
+++ AdventOfCode2016/Solutions/Day15.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2016.Solutions
 {
@@ -22,11 +23,50 @@ namespace AdventOfCode2016.Solutions
                 Init(Inputs.Day15.realData);
             }
         }
+        
+        public int CountDiscRotations(string[] lines)
+        {
+            var discs = new List<int[]>();
+            var pattern = @"Disc #[0-9]+ has (?<maxPosition>[0-9]+) positions; at time=0, it is at position (?<startPosition>[0-9]+).";
+            var rgx = new Regex(pattern);
+            foreach (var line in lines) {
+                Match match = rgx.Match(line);
+                if (match.Success) {
+                    discs.Add(new int[2] { int.Parse(match.Groups["startPosition"].Value), int.Parse(match.Groups["maxPosition"].Value) - 1 });
+                }
+            }
+
+            var passCount = 0;
+            var rotations = -1;
+            while (passCount < discs.Count) {
+                passCount = 0;
+                rotations++;
+                var j = 0;
+                foreach (var disc in discs) {
+                    j++;
+
+                    var actualSteps = ((rotations + j) % (disc[1] + 1));
+                    var currentPos = disc[0] + actualSteps;
+                    if (currentPos > disc[1]) {
+                        currentPos -= disc[1] + 1;
+                    }
+                    if (currentPos == 0) {
+                        passCount++;
+                    }
+                }
+            }
+            return rotations;
+        }
 
         public override void Run()
         {
             base.Run();
 
+            firstResult = CountDiscRotations(inputs).ToString();
+
+            var newInputs = (input + "\nDisc #7 has 11 positions; at time=0, it is at position 0.").Split('\n');
+
+            secondResult = CountDiscRotations(newInputs).ToString();  
         }
     }
 }
