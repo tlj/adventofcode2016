463e40b6f557fb0da53cca4ba68eacd7b2a7c394
Thomas L. Johnsen <t.johnsen@sportradar.com>
2016-12-16 08:30:00 +0100

    Day 16

--- AdventOfCode2016/Inputs/Day16.cs
+++ AdventOfCode2016/Inputs/Day16.cs
@@ -3,7 +3,7 @@ namespace AdventOfCode2016.Inputs
 {
     public class Day16
     {
-        public static string testData = @"";
-        public static string realData = @"";
+        public static string testData = @"110010110100";
+        public static string realData = @"11011110011011101";
     }
 }
