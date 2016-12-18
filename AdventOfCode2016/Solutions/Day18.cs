d6702f7e75e5219fb2b98847869b3e5b31de9e7b
tlj <tlj@tlj.no>
2016-12-18 15:56:33 +0100

    Day 18

--- AdventOfCode2016/Solutions/Day18.cs
+++ AdventOfCode2016/Solutions/Day18.cs
@@ -5,6 +5,8 @@ namespace AdventOfCode2016.Solutions
 {
     public class Day18 : Base
     {
+        const char TrapCharacter = '^';
+        const char SafeCharacter = '.';
 
         public Day18(string dayInput)
         {
@@ -23,10 +25,51 @@ namespace AdventOfCode2016.Solutions
             }
         }
 
+        public static string GetRow(string previousRow)
+        {
+            var newRow = "";
+
+            for (var i = 0; i < previousRow.Length; i++)
+            {
+                bool leftTileTrap = (i > 0) && previousRow[i - 1] == TrapCharacter;
+                bool centerTileTrap = previousRow[i] == TrapCharacter;
+                bool rightTileTrap = (i < previousRow.Length - 1) && previousRow[i + 1] == TrapCharacter;
+
+                if (
+                    (leftTileTrap && centerTileTrap && !rightTileTrap) || 
+                    (centerTileTrap && rightTileTrap && !leftTileTrap) ||
+                    (leftTileTrap && !centerTileTrap && !rightTileTrap) ||
+                    (rightTileTrap && !centerTileTrap && !leftTileTrap))
+                {
+                    newRow += TrapCharacter;
+                } else
+                {
+                    newRow += SafeCharacter;
+                }
+
+            }
+
+            return newRow;
+        }
+
+        public static int GetSafeCount(string startRow, int rows)
+        {
+            var safeCount = startRow.Split('.').Length - 1;
+            var row = startRow;
+            for (var i = 1; i < rows; i++)
+            {
+                row = GetRow(row);
+                safeCount += row.Split('.').Length - 1;
+            }
+            return safeCount;
+        }
+
         public override void Run()
         {
             base.Run();
 
+            firstResult = GetSafeCount(input, 40).ToString();
+            secondResult = GetSafeCount(input, 400000).ToString();
         }
     }
 }
---
+++ AdventOfCode2016Tests/Tests/Day18Tests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using AdventOfCode2016.Solutions;
+
+namespace AdventOfCode2016Tests
+{
+
+    public class Day18Tests
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            var input       = @"..^^.";
+            var expected    = @".^^^^";
+            var actual = Day18.GetRow(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Example2Test()
+        {
+            var input = @".^^^^";
+            var expected = @"^^..^";
+            var actual = Day18.GetRow(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TenRowColTest()
+        {
+            Assert.Equal(38, Day18.GetSafeCount(AdventOfCode2016.Inputs.Day18.testData, 10));
+        }
+    }
+
+}
