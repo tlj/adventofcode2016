0407a50f109d0e70db9c800ae125ddae7569ccb4
Thomas L. Johnsen <t.johnsen@sportradar.com>
2016-12-19 10:53:56 +0100

    Day19

--- AdventOfCode2016/Inputs/Day19.cs
+++ AdventOfCode2016/Inputs/Day19.cs
@@ -3,7 +3,7 @@ namespace AdventOfCode2016.Inputs
 {
     public class Day19
     {
-        public static string testData = @"";
-        public static string realData = @"";
+        public static string testData = @"5";
+        public static string realData = @"3014603";
     }
 }
--- AdventOfCode2016/Solutions/Day19.cs
+++ AdventOfCode2016/Solutions/Day19.cs
@@ -23,10 +23,53 @@ namespace AdventOfCode2016.Solutions
             }
         }
 
+        public static int GetNextElf(int currentElf, int elfCount)
+        {
+            var elf = currentElf + 1;
+            if (elf > elfCount) {
+                elf = 1;
+            }
+            return elf;
+        }
+
+        public static int GetElfWithPresentPart1(int elfCount)
+        {
+            var elfs = new Dictionary<int, int>();
+            for (var i = 1; i <= elfCount; i++) {
+                elfs.Add(i, 1);
+            }
+            var end = false;
+            while (!end) {
+                for (var i = 1; i <= elfCount; i++) {
+                    if (elfs[i] == 0) continue;
+                    var nextElf = GetNextElf(i, elfCount);
+                    while (nextElf != i) {
+                        if (elfs[nextElf] > 0) {
+                            elfs[i] += elfs[nextElf];
+                            elfs[nextElf] = 0;
+                            break;
+                        }
+                        nextElf = GetNextElf(nextElf, elfCount);
+                    }
+                    if (nextElf == i) {
+                        end = true;
+                    }
+                }
+            }
+
+            foreach (var elf in elfs) {
+                if (elf.Value > 0) {
+                    return elf.Key;
+                }
+            }
+            return 0;
+        }
+
         public override void Run()
         {
             base.Run();
 
+            firstResult = GetElfWithPresentPart1(int.Parse(input)).ToString();
         }
     }
 }
