1ec5201aef348b0f32dc63d58930664445a0d032
tlj <tlj@tlj.no>
2016-12-20 07:12:30 +0100

    Day 20, part 1, part of part2

--- AdventOfCode2016/Solutions/Day20.cs
+++ AdventOfCode2016/Solutions/Day20.cs
@@ -23,10 +23,68 @@ namespace AdventOfCode2016.Solutions
             }
         }
 
+        public string Part1()
+        {
+            uint highLow = uint.MinValue;
+
+            var unchanged = false;
+            while (!unchanged)
+            {
+                unchanged = true;
+                foreach (var l in inputs)
+                {
+                    var vals = l.Split('-');
+                    if (uint.Parse(vals[1]) >= highLow && uint.Parse(vals[0]) <= highLow)
+                    {
+                        highLow = uint.Parse(vals[1]) + 1;
+                        unchanged = false;
+                    }
+                }
+            }
+            return highLow.ToString();
+        }
+
+        public class SortNumeric : IComparable<uint[]>
+        {
+            public int CompareTo(uint[] comparePart)
+            {
+                
+                return 0;
+            }
+        }
+
         public override void Run()
         {
             base.Run();
 
+            firstResult = Part1();
+
+            var listInputs = new List<uint[]>();
+            foreach (var l in inputs)
+            {
+                var vals = l.Split('-');
+                listInputs.Add(new uint[2] { uint.Parse(vals[0]), uint.Parse(vals[1]) });
+            }
+            listInputs.Sort(delegate (uint[] x, uint[] y)
+            {
+                return x[0].CompareTo(y[0]);
+            });
+
+            var allowed = uint.MaxValue;
+            uint lastEnd = 0;
+
+            foreach (var l in listInputs)
+            {
+                if (l[0] > lastEnd || allowed == uint.MaxValue)
+                {
+                    allowed -= (l[1] - l[0]);
+                } else if (lastEnd > l[0])
+                {
+                    allowed -= (l[1] - l[0]) + (lastEnd - l[0]);
+                }
+            }
+
+            secondResult = allowed.ToString();           
         }
     }
 }
